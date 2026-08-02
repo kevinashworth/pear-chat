1cf14ee28150621e9265a19dc2f1445e8520cc45
Kevin Ashworth <1218844+kevinashworth@users.noreply.github.com>
2026-08-02 14:32:52 -0700

    lint and format

--- schema.js
+++ schema.js
@@ -1,55 +1,53 @@
-const Hyperschema = require('hyperschema')
-const HyperdbBuilder = require('hyperdb/builder')
-const Hyperdispatch = require('hyperdispatch')
+const Hyperschema = require("hyperschema");
+const HyperdbBuilder = require("hyperdb/builder");
+const Hyperdispatch = require("hyperdispatch");
 
-const SCHEMA_DIR = './spec/schema'
-const DB_DIR = './spec/db'
-const DISPATCH_DIR = './spec/dispatch'
+const SCHEMA_DIR = "./spec/schema";
+const DB_DIR = "./spec/db";
+const DISPATCH_DIR = "./spec/dispatch";
 
-const hyperSchema = Hyperschema.from(SCHEMA_DIR)
-const schema = hyperSchema.namespace('pear-chat')
+const hyperSchema = Hyperschema.from(SCHEMA_DIR);
+const schema = hyperSchema.namespace("pear-chat");
 schema.register({
-  name: 'writer',
-  fields: [
-    { name: 'key', type: 'buffer', required: true }
-  ]
-})
+  name: "writer",
+  fields: [{ name: "key", type: "buffer", required: true }],
+});
 schema.register({
-  name: 'invite',
+  name: "invite",
   fields: [
-    { name: 'id', type: 'buffer', required: true },
-    { name: 'invite', type: 'buffer', required: true },
-    { name: 'publicKey', type: 'buffer', required: true },
-    { name: 'expires', type: 'int', required: true }
-  ]
-})
+    { name: "id", type: "buffer", required: true },
+    { name: "invite", type: "buffer", required: true },
+    { name: "publicKey", type: "buffer", required: true },
+    { name: "expires", type: "int", required: true },
+  ],
+});
 schema.register({
-  name: 'message',
+  name: "message",
   fields: [
-    { name: 'id', type: 'string', required: true },
-    { name: 'text', type: 'string', required: true },
-    { name: 'info', type: 'json' }
-  ]
-})
-Hyperschema.toDisk(hyperSchema)
+    { name: "id", type: "string", required: true },
+    { name: "text", type: "string", required: true },
+    { name: "info", type: "json" },
+  ],
+});
+Hyperschema.toDisk(hyperSchema);
 
-const hyperdb = HyperdbBuilder.from(SCHEMA_DIR, DB_DIR)
-const db = hyperdb.namespace('pear-chat')
+const hyperdb = HyperdbBuilder.from(SCHEMA_DIR, DB_DIR);
+const db = hyperdb.namespace("pear-chat");
 db.collections.register({
-  name: 'invites',
-  schema: '@pear-chat/invite',
-  key: ['id']
-})
+  name: "invites",
+  schema: "@pear-chat/invite",
+  key: ["id"],
+});
 db.collections.register({
-  name: 'messages',
-  schema: '@pear-chat/message',
-  key: ['id']
-})
-HyperdbBuilder.toDisk(hyperdb)
+  name: "messages",
+  schema: "@pear-chat/message",
+  key: ["id"],
+});
+HyperdbBuilder.toDisk(hyperdb);
 
-const hyperdispatch = Hyperdispatch.from(SCHEMA_DIR, DISPATCH_DIR, { offset: 0 })
-const dispatch = hyperdispatch.namespace('pear-chat')
-dispatch.register({ name: 'add-writer', requestType: '@pear-chat/writer' })
-dispatch.register({ name: 'add-invite', requestType: '@pear-chat/invite' })
-dispatch.register({ name: 'add-message', requestType: '@pear-chat/message' })
-Hyperdispatch.toDisk(hyperdispatch)
+const hyperdispatch = Hyperdispatch.from(SCHEMA_DIR, DISPATCH_DIR, { offset: 0 });
+const dispatch = hyperdispatch.namespace("pear-chat");
+dispatch.register({ name: "add-writer", requestType: "@pear-chat/writer" });
+dispatch.register({ name: "add-invite", requestType: "@pear-chat/invite" });
+dispatch.register({ name: "add-message", requestType: "@pear-chat/message" });
+Hyperdispatch.toDisk(hyperdispatch);
--- workers/worker-task.js
+++ workers/worker-task.js
@@ -1,70 +1,70 @@
-const Corestore = require('corestore')
-const debounce = require('debounceify')
-const Hyperswarm = require('hyperswarm')
-const ReadyResource = require('ready-resource')
+const Corestore = require("corestore");
+const debounce = require("debounceify");
+const Hyperswarm = require("hyperswarm");
+const ReadyResource = require("ready-resource");
 
-const ChatRoom = require('./chat-room')
+const ChatRoom = require("./chat-room");
 
 class WorkerTask extends ReadyResource {
-  constructor (pipe, storage, opts = {}) {
-    super()
+  constructor(pipe, storage, opts = {}) {
+    super();
 
-    this.pipe = pipe
-    this.storage = storage
-    this.invite = opts.invite
-    this.name = opts.name || `User ${Date.now()}`
-    this.peers = 0
+    this.pipe = pipe;
+    this.storage = storage;
+    this.invite = opts.invite;
+    this.name = opts.name || `User ${Date.now()}`;
+    this.peers = 0;
 
-    this.store = new Corestore(storage)
-    this.swarm = new Hyperswarm()
-    this.swarm.on('connection', (conn) => {
-      this.store.replicate(conn)
-      this._peers(1)
-      conn.once('close', () => this._peers(-1))
-    })
+    this.store = new Corestore(storage);
+    this.swarm = new Hyperswarm();
+    this.swarm.on("connection", (conn) => {
+      this.store.replicate(conn);
+      this._peers(1);
+      conn.once("close", () => this._peers(-1));
+    });
 
-    this.room = new ChatRoom(this.store, this.swarm, this.invite)
-    this.debounceMessages = debounce(() => this._messages())
-    this.room.on('update', () => this.debounceMessages())
+    this.room = new ChatRoom(this.store, this.swarm, this.invite);
+    this.debounceMessages = debounce(() => this._messages());
+    this.room.on("update", () => this.debounceMessages());
   }
 
-  async _open () {
-    await this.store.ready()
-    await this.room.ready()
+  async _open() {
+    await this.store.ready();
+    await this.room.ready();
 
-    this.pipe.on('data', async (data) => {
-      let message
+    this.pipe.on("data", async (data) => {
+      let message;
       try {
-        message = JSON.parse(data)
+        message = JSON.parse(data);
       } catch {
-        return
+        return;
       }
-      if (message.type === 'add-message') {
-        await this.room.addMessage(message.text, { name: this.name, at: Date.now() })
+      if (message.type === "add-message") {
+        await this.room.addMessage(message.text, { name: this.name, at: Date.now() });
       }
-    })
+    });
 
     // Push the room invite so the renderer can show its "copy invite" button.
-    this.pipe.write(JSON.stringify({ type: 'invite', invite: await this.room.getInvite() }))
-    await this.debounceMessages()
+    this.pipe.write(JSON.stringify({ type: "invite", invite: await this.room.getInvite() }));
+    await this.debounceMessages();
   }
 
-  async _close () {
-    await this.room.close()
-    await this.swarm.destroy()
-    await this.store.close()
+  async _close() {
+    await this.room.close();
+    await this.swarm.destroy();
+    await this.store.close();
   }
 
-  _peers (delta) {
-    this.peers += delta
-    this.pipe.write(JSON.stringify({ type: 'peers', count: this.peers }))
+  _peers(delta) {
+    this.peers += delta;
+    this.pipe.write(JSON.stringify({ type: "peers", count: this.peers }));
   }
 
-  async _messages () {
-    const messages = await this.room.getMessages()
-    messages.sort((a, b) => a.info.at - b.info.at)
-    this.pipe.write(JSON.stringify({ type: 'messages', messages }))
+  async _messages() {
+    const messages = await this.room.getMessages();
+    messages.sort((a, b) => a.info.at - b.info.at);
+    this.pipe.write(JSON.stringify({ type: "messages", messages }));
   }
 }
 
-module.exports = WorkerTask
+module.exports = WorkerTask;
